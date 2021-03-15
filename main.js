153999ce2214d395141458f1add9e032c0f8ea64
Bartosz Jaroszkowski <bjaroszkowski@gmail.com>
2021-03-15 23:34:43 +0100

    First version of CV/Portfolio

---
+++ main.js
@@ -0,0 +1,22 @@
+$(function () {
+  menu = $(".menu");
+
+  $(".hamburger").on("click", function (e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function () {
+    var w = $(this).width();
+    if (w > 720) {
+      menu.removeAttr("style");
+    }
+  });
+
+  $("nav li").on("click", function (e) {
+    var w = $(window).width();
+    if (w < 720) {
+      menu.slideToggle();
+    }
+  });
+});
